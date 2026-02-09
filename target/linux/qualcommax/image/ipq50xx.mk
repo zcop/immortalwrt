b9180fa51f8c1780098ca92d6c2c2f791f214077
zcop <hongson.hn@gmail.com>
2026-02-09 19:21:07 +0000

    ipq50xx: add CR881x/AX3000 support and ath11k fixes

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -165,6 +165,39 @@ define Device/linksys_spnmx56
 endef
 TARGET_DEVICES += linksys_spnmx56
 
+define Device/redmi_ax3000
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	SOC := ipq5000
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX3000
+	DEVICE_ALT0_VENDOR := Xiaomi
+	DEVICE_ALT0_MODEL := CR880X
+	DEVICE_ALT0_VARIANT := (M81 version)
+	DEVICE_ALT1_VENDOR := Xiaomi
+	DEVICE_ALT1_MODEL := CR880X
+	DEVICE_ALT1_VARIANT := (M79 version)
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@mp02.1
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax3000
+endef
+TARGET_DEVICES += redmi_ax3000
+
+define Device/xiaomi_cr881x
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	SOC := ipq5018
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := CR881x
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@mp03.3
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_cr881x ath11k-firmware-ipq5018-qcn6122 \
+		ethtool ip-full tcpdump phytool mii-tool luci-app-portstatus kmod-net-dsa
+endef
+TARGET_DEVICES += xiaomi_cr881x
+
 define Device/xiaomi_ax6000
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
